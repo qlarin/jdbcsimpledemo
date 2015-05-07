8eb60dbabea7fc38ba400d505db1387ec569e6af
s11617 <st.sledziona@gmail.com>
2015-05-07 15:40:33 +0200

    delete query has been added to demo

--- src/main/java/domain/SQLdemo.java
+++ src/main/java/domain/SQLdemo.java
@@ -10,6 +10,7 @@ public class SQLdemo {
 		String insert = "Insert into authors " + " (name) "
 				+ " values ('John Deep')";
 		String update = "Update authors set name='John Marks' where id=11";
+		String delete = "Delete from authors where id=9";
 
 		try {
 
@@ -17,13 +18,18 @@ public class SQLdemo {
 					"jdbc:mysql://localhost:3306/testdb", "user1", "user1");
 			Statement stm = myConn.createStatement();
 
+			// Delete Query
+			int rowsAffected = stm.executeUpdate(delete);
+			System.out.println("Rows affected: " + rowsAffected);
+			System.out.println("Delete complete! And this is the current table");
+			
 			// Update Query
-			stm.executeUpdate(update);
-			System.out.println("Update complete! Take a look at the result. ");
+			/*stm.executeUpdate(update);
+			System.out.println("Update complete! Take a look at the result. ");*/
 			
 			// Insert Query
-			/* stm.executeUpdate(insert);
-			System.out.println("Insert complete! Look at this, what is changed? "); */
+			/*stm.executeUpdate(insert);
+			System.out.println("Insert complete! Look at this, what is changed? ");*/
 			 
 			// Select Query
 			ResultSet rs = stm.executeQuery(select);
