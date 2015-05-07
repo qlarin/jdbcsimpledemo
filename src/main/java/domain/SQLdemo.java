14af273ddb518f4b7a751ff9bab8f4484a799829
s11617 <st.sledziona@gmail.com>
2015-05-07 15:30:42 +0200

    update query has been added to demo

--- src/main/java/domain/SQLdemo.java
+++ src/main/java/domain/SQLdemo.java
@@ -5,28 +5,35 @@ import java.sql.*;
 public class SQLdemo {
 
 	public static void main(String[] args) {
-		
+
 		String select = "Select * from authors";
-		String insert = "Insert into authors " 
-				+ " (name) "
+		String insert = "Insert into authors " + " (name) "
 				+ " values ('John Deep')";
+		String update = "Update authors set name='John Marks' where id=11";
 
-		try{
-					
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "user1", "user1");
+		try {
+
+			Connection myConn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/testdb", "user1", "user1");
 			Statement stm = myConn.createStatement();
-		
-			stm.executeUpdate(insert);
-			System.out.println("Insert complete! Look at this, what is changed? ");
 
-			ResultSet rs  = stm.executeQuery(select);
+			// Update Query
+			stm.executeUpdate(update);
+			System.out.println("Update complete! Take a look at the result. ");
 			
-			while(rs.next()){
-				System.out.println(rs.getString("id") + " " + rs.getString("name"));
+			// Insert Query
+			/* stm.executeUpdate(insert);
+			System.out.println("Insert complete! Look at this, what is changed? "); */
+			 
+			// Select Query
+			ResultSet rs = stm.executeQuery(select);
+
+			while (rs.next()) {
+				System.out.println(rs.getString("id") + " "
+						+ rs.getString("name"));
 			}
-			
-		}
-		catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
