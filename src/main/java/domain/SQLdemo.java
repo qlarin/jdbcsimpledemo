b70e9d888ce82ee04220f7b1b538d69a608365ad
s11617 <st.sledziona@gmail.com>
2015-05-07 15:02:19 +0200

    insert query has been added to demo

--- src/main/java/domain/SQLdemo.java
+++ src/main/java/domain/SQLdemo.java
@@ -6,12 +6,20 @@ public class SQLdemo {
 
 	public static void main(String[] args) {
 		
-		
+		String select = "Select * from authors";
+		String insert = "Insert into authors " 
+				+ " (name) "
+				+ " values ('John Deep')";
+
 		try{
-			
+					
 			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "user1", "user1");
 			Statement stm = myConn.createStatement();
-			ResultSet rs = stm.executeQuery("Select * from authors;");
+		
+			stm.executeUpdate(insert);
+			System.out.println("Insert complete! Look at this, what is changed? ");
+
+			ResultSet rs  = stm.executeQuery(select);
 			
 			while(rs.next()){
 				System.out.println(rs.getString("id") + " " + rs.getString("name"));
